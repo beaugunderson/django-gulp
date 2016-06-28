9fff3254d71ac112688dad5f9f4b6f9249610925
Joshua Blum <jblum18@gmail.com>
2016-06-28 19:16:40 -0400

    Fail collectstatic if gulp fails

--- django_gulp/management/commands/collectstatic.py
+++ django_gulp/management/commands/collectstatic.py
@@ -4,17 +4,19 @@ import subprocess
 from django.conf import settings
 from django.contrib.staticfiles.management.commands.collectstatic import \
     Command as BaseCommand
+from django.core.management.base import CommandError
 
 
 class Command(BaseCommand):
     """
     A version of collectstatic that runs `gulp build --production` first.
     """
+
     def handle(self, *args, **options):
         if options['dry_run']:
             return
 
-        popen_args = {
+        popen_kwargs = {
             'shell': True,
             'stdin': subprocess.PIPE,
             'stdout': self.stdout,
@@ -25,14 +27,17 @@ class Command(BaseCommand):
         # when it's executed from Heroku... Ideally we wouldn't need any
         # Heroku-specific code for this to work.
         if os.path.exists('/app/requirements.txt'):
-            popen_args['env'] = {
+            popen_kwargs['env'] = {
                 'PATH': (os.environ['PATH'] +
                          ':/app/node_modules/.bin' +
                          ':/app/.heroku/node/bin')
             }
 
-        gulp_command = getattr(settings, 'GULP_PRODUCTION_COMMAND', 'gulp build --production')
-        subprocess.Popen(gulp_command,
-                         **popen_args).wait()
+        gulp_command = getattr(
+            settings, 'GULP_PRODUCTION_COMMAND', 'gulp build --production')
+        try:
+            subprocess.check_call(gulp_command, **popen_kwargs)
+        except subprocess.CalledProcessError as e:
+            raise CommandError(e)
 
         super(Command, self).handle(*args, **options)
